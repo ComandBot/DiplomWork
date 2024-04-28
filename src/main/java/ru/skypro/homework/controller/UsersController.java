2c6b491a9d2957824c7459f1f0f6fc332fb0d13b
Sergey Volkov <rusobraz@mail.ru>
2024-04-28 07:22:59 +0300

    Добавил контроллер. Вопрос по работе контроллера.2

--- src/main/java/ru/skypro/homework/controller/UsersController.java
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -1,6 +1,13 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,27 +16,96 @@ import ru.skypro.homework.dto.NewPassword;
 import ru.skypro.homework.dto.UpdateUser;
 import ru.skypro.homework.dto.User;
 
+import java.io.IOException;
+
 @Slf4j
 @RestController
+@CrossOrigin(value = "http://localhost:3000")
 @RequestMapping("/users")
 public class UsersController {
+
+    @Operation(
+            tags = "Пользователи",
+            summary = "Обновление пароля",
+            operationId = "setPassword",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = NewPassword.class
+                            )
+                    )
+            ),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "403", description = "Forbidden")
+            }
+
+    )
     @PostMapping("/set_password")
     public ResponseEntity<?> setPassword(@RequestBody NewPassword newPassword) {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(
+            tags = "Пользователи",
+            summary = "Получение информации об авторизованном пользователе",
+            operationId = "getUser",
+            responses = {@ApiResponse(responseCode = "200", description = "OK", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(
+                            implementation = User.class
+                    )
+            )),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized")
+            }
+
+    )
     @GetMapping("/me")
     public ResponseEntity<User> getUser() {
         return ResponseEntity.ok(new User());
     }
 
+    @Operation(
+            tags = "Пользователи",
+            summary = "Обновление информации об авторизованном пользователе",
+            operationId = "updateUser",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = UpdateUser.class
+                            )
+                    )
+            ),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized")
+            }
+
+    )
     @PatchMapping("/me")
     public ResponseEntity<UpdateUser> updateUser(@RequestBody UpdateUser updateUser) {
         return ResponseEntity.ok(updateUser);
     }
 
+    @Operation(
+            tags = "Пользователи",
+            summary = "Обновление аватара авторизованного пользователя",
+            operationId = "updateUserImage",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = byte.class))
+                    )
+            ),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized")
+            }
+
+    )
     @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> updateUserImage(@RequestParam MultipartFile image) {
+    public ResponseEntity<?> updateUserImage(@RequestPart MultipartFile image) throws IOException {
+        byte[] file = image.getBytes();
         return ResponseEntity.ok().build();
     }
 
--- src/main/java/ru/skypro/homework/dto/NewPassword.java
+++ src/main/java/ru/skypro/homework/dto/NewPassword.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 public class NewPassword {
+
     private String currentPassword;
     private String newPassword;
 }
