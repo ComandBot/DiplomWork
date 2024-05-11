06c0e60cf3710b6f4cd2c66f363164bfba3b60ba
Galina684 <galina684@gmail.com>
2024-05-11 22:26:09 +0300

    3 неделя. Сервисы

--- src/main/java/ru/skypro/homework/service/AdService.java
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -1,4 +1,17 @@
 package ru.skypro.homework.service;
 
+import ru.skypro.homework.dto.AdDto;
+import ru.skypro.homework.dto.AdsDto;
+import ru.skypro.homework.dto.CreateOrUpdateAdDto;
+
+import java.util.Optional;
+
 public interface AdService {
+    AdDto addAd(CreateOrUpdateAdDto newAd);
+    Optional<AdDto> findAllAds();
+    Optional<AdDto> findAdsByAuthUser();
+    AdDto updateAd(CreateOrUpdateAdDto newAd);
+    void deleteAd();
+
+
 }
--- src/main/java/ru/skypro/homework/service/impl/AdServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AdServiceImpl.java
@@ -2,19 +2,54 @@ package ru.skypro.homework.service.impl;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.AdDto;
+import ru.skypro.homework.dto.CreateOrUpdateAdDto;
 import ru.skypro.homework.repository.AdRepository;
 import ru.skypro.homework.service.AdService;
 import ru.skypro.homework.service.UserService;
+import ru.skypro.homework.mapper.AdMapperService;
+
+import java.util.Optional;
 
 @Service
 public class AdServiceImpl implements AdService {
-    @Value(value = "${photo.dir.path}")
-    private String photoDir;
+//    @Value(value = "${photo.dir.path}")
+//    private String photoDir;
     private final AdRepository adRepository;
     private final UserService userService;
 
-    public AdServiceImpl(AdRepository adRepository, UserService userService) {
+    private final AdMapperService adMapperService;
+
+
+    public AdServiceImpl(AdRepository adRepository, UserService userService, AdMapperService adMapperService) {
         this.adRepository = adRepository;
         this.userService = userService;
+        this.adMapperService = adMapperService;
+    }
+
+
+    @Override
+    public AdDto addAd(CreateOrUpdateAdDto newAd) {
+        return null;
+    }
+
+    @Override
+    public Optional<AdDto> findAllAds() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<AdDto> findAdsByAuthUser() {
+        return Optional.empty();
+    }
+
+    @Override
+    public AdDto updateAd(CreateOrUpdateAdDto newAd) {
+        return null;
+    }
+
+    @Override
+    public void deleteAd() {
+
     }
 }
